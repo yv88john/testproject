f0ced15630a7926eb66e0d5cbdc641f404645669
John Lui (AI) <johnlui@srl-hk.intra.net>
2017-06-30 17:05:58 +0800

    forminputvalue

--- App.js
+++ App.js
@@ -1,64 +1,23 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading } from 'expo';
-import { FontAwesome } from '@expo/vector-icons';
-import RootNavigation from './navigation/RootNavigation';
+import React, { Component } from 'react';
+import { Container, Content, Form, Item, Input, Label } from 'native-base';
 
-import cacheAssetsAsync from './utilities/cacheAssetsAsync';
-
-export default class AppContainer extends React.Component {
-  state = {
-    appIsReady: false,
-  };
-
-  componentWillMount() {
-    this._loadAssetsAsync();
-  }
-
-  async _loadAssetsAsync() {
-    try {
-      await cacheAssetsAsync({
-        images: [require('./assets/images/expo-wordmark.png')],
-        fonts: [
-          FontAwesome.font,
-          { 'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf') },
-        ],
-      });
-    } catch (e) {
-      console.warn(
-        'There was an error caching assets (see: main.js), perhaps due to a ' +
-          'network timeout, so we skipped caching. Reload the app to try again.'
-      );
-      console.log(e.message);
-    } finally {
-      this.setState({ appIsReady: true });
-    }
-  }
-
+export default class FixedLabelExample extends Component {
   render() {
-    if (this.state.appIsReady) {
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          {Platform.OS === 'android' &&
-            <View style={styles.statusBarUnderlay} />}
-          <RootNavigation />
-            <Text>Hello Wor</Text>
-        </View>
-      );
-    } else {
-      return <AppLoading />;
-    }
+    return (
+      <Container>
+        <Content>
+          <Form>
+            <Item fixedLabel>
+              <Label>Username</Label>
+              <Input />
+            </Item>
+            <Item fixedLabel last>
+              <Label>Password</Label>
+              <Input />
+            </Item>
+          </Form>
+        </Content>
+      </Container>
+    );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  statusBarUnderlay: {
-    height: 24,
-    backgroundColor: 'rgba(0,0,0,0.2)',
-  },
-});
